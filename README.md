a3de64038c86643250b0d0c9dd395d4953fc1fe1
gamabreno <brenogama97@gmail.com>
2025-10-02 06:42:18 -0500

    adicionaod conteudo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bgsite
+
+https://gamabreno.github.io/bgsite/
--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function mudar_conteudo_elemento(){
 
     document.getElementById("demo").innerHTML = "Oclique está funcionando";
 }
+
+function mudar_cor_elemento(){
+
+    const paragrafo = document.querySelector('#paragrafo');
+
+    paragrafo.addEventListener('click', ()=> {
+
+        const elemento_paragrafo = document.querySelector('#paragrafo');
+
+        elemento_paragrafo.style.color = 'red';
+    });
+}
